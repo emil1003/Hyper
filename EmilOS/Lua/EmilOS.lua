3970da6949524a99fc386c9fa30fc4b1b2a5cac1
emil1003 <columbus-pc@hotmail.com>
2018-09-06 23:47:30 +0200

    Add initial dialog support

--- EmilOS/Lua/EmilOS.lua
+++ EmilOS/Lua/EmilOS.lua
@@ -21,7 +21,9 @@ end
 
 -- Get nodes and organize them
 local Topbar = {
-	container = getNode "#Topbar",
+	self = getNode "#Topbar",
+
+	menuButton = getNode "#TopbarMenuButton",
 
 	clock = getNode "#TopbarClock",
 	searchButton = getNode "#TopbarSearchButton",
@@ -29,26 +31,52 @@ local Topbar = {
 }
 
 local Sidebar = {
-	container = getNode "#Sidebar",
+	self = getNode "#Sidebar",
 
 	tabContainer = getNode "#SidebarTabContainer",
 
 	Notifications = {
-		container = getNode "#SidebarNotifications"
+		self = getNode "#SidebarNotifications"
 	},
 
 	isOpen = false
 }
 
+local Dialog = {
+	self = getNode "#Dialog",
+
+	label = getNode "#DialogLabel",
+
+	rightButton = getNode "#DialogButtonRight",
+	leftButton = getNode "#DialogButtonLeft"
+}
+
 -- Stupid fixes that shouldn't have to be done like this, but it's the only way.
 function Sidebar:toggle() -- Can't be inside Sidebar because reasons
 	if Sidebar.isOpen then
-		Sidebar.container:animate( "showSidebar", "X", App.width + 1, 0.3, "inCubic" )
+		Sidebar.self:animate( "showSidebar", "X", App.width + 1, 0.3, "inCubic" )
 	else
-		Sidebar.container:animate( "hideSidebar", "X", App.width - Sidebar.container.width + 1, 0.3, "outCubic" )
+		Sidebar.self:animate( "hideSidebar", "X", App.width - Sidebar.self.width + 1, 0.3, "outCubic" )
 	end
 	Sidebar.isOpen = not Sidebar.isOpen
 end
+
+function Dialog:create( label, buttonRight, buttonLeft, funcRight, funcLeft )
+	Dialog.label:setText( label )
+	Dialog.rightButton:setText( buttonRight )
+	Dialog.rightButton:on( "trigger", function()
+		Dialog.self:animate( "dialog", "Y", App.height + 1, 0.3, "inCubic" )
+		if funcRight then funcRight() end
+	end)
+	Dialog.leftButton:setText( buttonLeft and buttonLeft or "" )
+	Dialog.leftButton:on( "trigger", function()
+		Dialog.self:animate( "dialog", "Y", App.height + 1, 0.3, "inCubic" )
+		if funcLeft then funcLeft() end
+	end)
+
+	Dialog.self:animate( "dialog", "Y", (App.height - Dialog.self.height) / 2, 0.3, "outCubic" )
+end
+
 
 Sidebar.tabContainer:set({ -- TabbedPageContainer doesn't apply colors from theme
 	tabColour = colors.black,
@@ -58,6 +86,26 @@ Sidebar.tabContainer:set({ -- TabbedPageContainer doesn't apply colors from them
 })
 
 -- Setup event handlers
+Topbar.menuButton:on( "trigger", function()
+	if Topbar.Menu then
+		App:removeNode( Topbar.Menu )
+	end
+	Topbar.Menu = App:addNode( ContextMenu({
+		{"button", " Om EmilOS ", function() end},
+		{"rule"},
+		{"button", " Indstillinger ", function() end},
+		{"rule"},
+		{"menu", " Afslut...   \16 ", {
+			{"button", " Log af ", function()
+				App:removeNode( Topbar.Menu )
+				Dialog:create( "Vil du logge af?", "Ja", "Nej", function() App:stop() end )
+			end},
+			{"button", " Genstart ", function() os.reboot() end},
+			{"button", " Luk ned ", function() os.shutdown() end}
+		}}
+	}, 2, 2 ))
+end)
+
 Sidebar.tabContainer:selectPage( "SidebarNotifications" )
 Topbar.sidebarButton:on( "trigger", Sidebar.toggle )
 
