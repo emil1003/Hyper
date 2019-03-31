34408058bd7f63701da8efd39d39e35256e2354d
emil1003 <columbus-pc@hotmail.com>
2019-03-31 01:29:27 +0100

    Add initial Setup program and UI

--- Hyper/Lua/Setup.lua
+++ Hyper/Lua/Setup.lua
@@ -5,4 +5,31 @@
 
 --TODO: Create setup application
 --For now, resume launching desktop
-return
+
+local termSize = {term.getSize()}
+
+-- Application Instance
+local App = Application():set({
+	width = termSize[1],
+	height = termSize[2],
+	terminatable = true,
+	color = colors.black,
+	backgroundColor = colors.white
+})
+
+local function getNode(id)
+	return App:query(id).result[1]
+end
+
+App:importFromTML("Hyper/UI/TML/Setup.tml")
+App:addTheme(Theme.fromFile("Hyper", "Hyper/UI/Themes/Hyper.theme"))
+App:addTheme(Theme.fromFile("Setup", "Hyper/UI/Themes/Setup.theme"))
+
+App:query("#SetupWindow"):on("close", function() App:stop() end)
+
+App:start()
+
+
+
+
+
