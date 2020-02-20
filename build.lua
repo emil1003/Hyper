5f10f862ec979806d72f3a72f226fc5ffcec5b40
emil1003 <columbus-pc@hotmail.com>
2020-02-20 13:43:53 +0100

    Add building scripts and GitHub Workflow

---
+++ build.lua
@@ -0,0 +1,25 @@
+
+unpack = unpack or table.unpack
+
+local args = {...}
+
+local opts = {
+	"--pickle-source=lib/Pickle.lua",
+	"--minify-source=lib/Minify.lua",
+	"--titanium-disable-check",
+	"--vfs-allow-raw-fs-access",
+	"--vfs-allow-raw-fs-access",
+	"--source=Hyper",
+	"--class-source=Hyper/Class",
+	"--init=Hyper/Lua/Hyper.lua",
+	"--pre-init=Hyper/Lua/pre-init.lua",
+	"--output=Hyper.tpkg"
+}
+
+for _, arg in pairs(args) do
+	if arg == "minify" or arg == "m" then
+		table.insert(opts, "--minify")
+	end
+end
+
+assert(loadfile("lib/package.lua"))(unpack(opts))
