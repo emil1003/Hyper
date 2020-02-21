5fd2259c249161e7d0fc9f120cdee98690879f38
emil1003 <columbus-pc@hotmail.com>
2020-02-21 21:53:52 +0100

    Add status reporting for build.lua

--- build.lua
+++ build.lua
@@ -1,8 +1,13 @@
 
+--Get lfs package
+local lfs = require("lfs")
+
+--Patch unpack on Lua >= 5.2
 unpack = unpack or table.unpack
 
 local args = {...}
 
+--Set default options
 local opts = {
 	"--pickle-source=lib/Pickle.lua",
 	"--minify-source=lib/Minify.lua",
@@ -16,10 +21,37 @@ local opts = {
 	"--output=Hyper.tpkg"
 }
 
+--Print build script info
+print("build.lua invoked")
+
+--Print working directory
+print("Working directory: "..lfs.currentdir())
+
+--Parse arguments
 for _, arg in pairs(args) do
 	if arg == "minify" or arg == "m" then
 		table.insert(opts, "--minify")
 	end
 end
 
-assert(loadfile("lib/package.lua"))(unpack(opts))
+--Print options
+print("Build options:")
+
+for _, opt in pairs(opts) do
+	print(opt)
+end
+
+--Execute
+print("Invoking lib/package.lua...")
+
+local package = assert(loadfile("lib/package.lua"))
+
+local state, out = pcall(package, unpack(opts))
+
+--Print status
+if state then
+	print("[ OK ] lib/package.lua succeeded")
+else
+	print("[FAIL] lib/package.lua encountered error:")
+	error(out)
+end
